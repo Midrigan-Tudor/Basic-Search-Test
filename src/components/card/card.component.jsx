b655f7175911ac6a8c78909de5bb15c725fa81e2
Midrigan-Tudor <midrigan.tudor@gmail.com>
2021-06-03 16:20:43 +0300

    second commit

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,12 +1,17 @@
 import React from "react"
 import PropTypes from 'prop-types';
-import "../card/card.styles.css"
+import "../card/card.styles.css";
+import styled from '@emotion/styled';
+
+const Name=styled.h2`
+color: #blue;
+`;
 
 const Card= props=>{
     return(
 <div className="card-container">
     <img alt='monster' src={`https://robohash.org/${props.user}?set=set2&size=180x180`}/>
-    <h2>{props.user}</h2>
+    <Name><h2>{props.user}</h2></Name>
 </div>);
 }
 
